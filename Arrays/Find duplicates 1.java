14ca95e0501c3241f13532c45983a9b6bb942f94
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-09-15 22:44:08 +0530

    Create Find duplicates 1.java

---
+++ Arrays/Find duplicates 1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of elements in array:");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter the elements:");
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		System.out.println("The duplicate elements are:");
+		for(int i=0;i<n;i++){
+		    for(int j=i+1;j<n;j++){
+		        if(a[i]==a[j]){
+		            System.out.print(a[i]+" ");
+		        }
+		    }
+		}
+	}
+}
