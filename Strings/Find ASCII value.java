7fb8cc44ed755a6fbb4479d385090264fa344744
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-09-17 23:00:49 +0530

    Create Find ASCII value.java

---
+++ Strings/Find ASCII value.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char c=sc.next().charAt(0);
+		int i=c;
+		System.out.println("ASCII value of "+c+" is "+i);
+	}
+}
