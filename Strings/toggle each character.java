9d02d514eaf92389c69483fcc305530649afcaaa
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-09-19 23:10:47 +0530

    Create toggle each character.java

---
+++ Strings/toggle each character.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str1 = sc.nextLine();
+        char str[] = str1.toCharArray();
+        int i = 0;
+
+        while (i < str1.length()) {
+            if (str[i] >= 'a' && str[i] <= 'z') {               
+                str[i] = (char)(str[i] + 'A' - 'a');           
+            } 
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] = (char)(str[i] + 'a' - 'A');
+            }
+            i++;
+        }
+
+        System.out.println("Toggled string: ");
+        for (i = 0; i < str1.length(); i++) {
+            System.out.print(str[i]);
+        }
+    }
+}
