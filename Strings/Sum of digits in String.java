3cb38407432de78fb5e7145d1811597b5c812f56
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2024-09-26 21:53:00 +0530

    Create Sum of digits in String.java

---
+++ Strings/Sum of digits in String.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextInt();
+    int sum=0;
+    for(int i=0;i<str.length();i++){ 
+      if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+        sum+=(str.charAt(i)-'0');
+        }
+    }
+    System.out.println("Sum of all digits " +sum );
+  }
+}
